831feb977e91fc0f8ea433d09082cb6fdf8fc079
chris <chris@CFANGS>
2021-08-30 19:04:45 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace timeLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i=0; i < number; i++)
+            {
+                Console.WriteLine((i+1) + "Abracadabra");
+            }
+            
+
+           
+        }
+    }
+}
